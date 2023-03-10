4eae09e4b376e0a280623612ce229e19af855744
Russell West <russellwwest@gmail.com>
2023-03-10 20:22:28 +0000

    Adds cache busting

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
 
+const hash = Math.floor(Math.random() * 90000) + 10000
+
 export default defineConfig({
   plugins: [
     laravel(['resources/js/app.jsx']),
     react()
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]' + hash + '.js',
+        chunkFileNames: '[name]' + hash + '.js',
+        assetFileNames: '[name]' + hash + '.[ext]'
+      }
+    }
+  }
 })
